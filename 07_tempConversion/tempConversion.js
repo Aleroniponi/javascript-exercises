2ef80110e62fa2520c194a8dec005b8bc0bbfc9f
Alexander Hansen <alex@Alexanders-MacBook-Air.local>
2022-04-28 18:01:50 -0700

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  return Math.floor(((farenheit - 32) * (5/9)) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return Math.floor(((celcius * (5/9)) + 32) / 10);
 };
 
 // Do not edit below this line
